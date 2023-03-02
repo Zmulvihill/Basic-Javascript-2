d62f0b359c963ced7cfc0269e53b8ca42bd03e88
Zmulvihill <zmulvihi@yahoo.com>
2023-03-02 16:33:01 -0700

    added script10 and ran node

---
+++ script10.js
@@ -0,0 +1,5 @@
+//Declare a Read-Only Variable with the const Keyword
+const FCC = "freeCodeCamp"; // Changed var to const and fCC to FCC
+let fact = "is cool!"; // Changed var to let
+fact = "is awesome!";
+console.log(FCC, fact); // Changed fCC to FCC
