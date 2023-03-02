27b5382532d23402f28ca390e1c7355361cd6f3e
Zmulvihill <zmulvihi@yahoo.com>
2023-03-02 13:19:23 -0700

    added script8 and ran node

---
+++ script8.js
@@ -0,0 +1,13 @@
+//Understanding Case Sensitivity in Variables
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+console.log(studlyCapVar);
+console.log(properCamelCase);
+console.log(titleCaseOver);
