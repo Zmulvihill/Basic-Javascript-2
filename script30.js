99d5ba5207499a24a0d5ccce32ce01384da60b66
Zmulvihill <zmulvihi@yahoo.com>
2023-03-03 10:53:06 -0700

    added script30 and ran node

---
+++ script30.js
@@ -0,0 +1,5 @@
+//Constructing Strings with Variables
+// Only change code below this line
+const myName = "Zack Mulvihill";
+const myStr = "My name is " + myName + " and I am well!";
+console.log(myStr)
