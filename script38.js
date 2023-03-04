9f8ec6425a69675ac9554e50fef1f57f4f5150ab
Zmulvihill <zmulvihi@yahoo.com>
2023-03-04 14:30:23 -0700

    added script38 and ran node

---
+++ script38.js
@@ -0,0 +1,11 @@
+//Word Blanks
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + "."; // Only change this line;
+
+// Only change code above this line
+console.log(wordBlanks)
