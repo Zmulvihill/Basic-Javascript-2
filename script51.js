65d26ef66e8e13b0b11fdd6f3c44ffbcecf12c84
Zmulvihill <zmulvihi@yahoo.com>
2023-03-05 13:52:00 -0700

    added script51 and ran node

---
+++ script51.js
@@ -0,0 +1,10 @@
+//Return a Value from a Function with Return
+function timesFive(num) {
+    return num * 5;
+  }
+  timesFive(5);
+  timesFive(2);
+  timesFive(0);
+  console.log(timesFive(5))
+  console.log(timesFive(2))
+  console.log(timesFive(0))
