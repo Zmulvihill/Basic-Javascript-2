5b4876321deb25707878fa67905ab7e08a87758d
Zmulvihill <zmulvihi@yahoo.com>
2023-03-06 10:13:18 -0700

    added script58, didn't run node

---
+++ script58.js
@@ -0,0 +1,8 @@
+//Understanding Boolean Values
+function welcomeToBooleans() {
+    // Only change code below this line
+  
+    return true; // Change this line, changed from false to true
+  
+    // Only change code above this line
+  }
