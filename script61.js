5c86576dbbeab76d0afb2074a91209f3e5859e23
Zmulvihill <zmulvihi@yahoo.com>
2023-03-06 10:25:50 -0700

    added script61, didn't run node

---
+++ script61.js
@@ -0,0 +1,10 @@
+//Comparison with the Strict Equality Operator
+// Setup
+function testStrict(val) {
+    if (val === 7) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  testStrict(10);
