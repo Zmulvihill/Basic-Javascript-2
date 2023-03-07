15bc5a102eb05f93cce7c3645c0c5312a9063309
Zmulvihill <zmulvihi@yahoo.com>
2023-03-07 11:21:44 -0700

    added script80 and ran node

---
+++ script80.js
@@ -0,0 +1,9 @@
+//Returning Boolean Values from Functions
+function isLess(a, b) {
+    // Only change code below this line
+    return a < b;
+    // Only change code above this line
+  }
+  
+  console.log(isLess(10, 15))
+  console.log(isLess(20, 15))
