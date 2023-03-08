fe3086861b02eeaae609c7e72e866cbf242dcfcd
Zmulvihill <zmulvihi@yahoo.com>
2023-03-08 16:57:03 -0700

    added script83 and ran node

---
+++ script83.js
@@ -0,0 +1,11 @@
+//Build JavaScript Objects
+const myDog = {
+    // Only change code below this line
+  name: "Cujo",
+  legs: 4,
+  tails: 1,
+  friends: ["Everyone", "Everything"]
+  
+    // Only change code above this line
+  };
+  console.log(myDog)
