fb8cca0d6ed18a1cd04ff39e12808f7f71185841
Zmulvihill <zmulvihi@yahoo.com>
2023-03-09 08:50:35 -0700

    added script92, didn't run node

---
+++ script92.js
@@ -0,0 +1,25 @@
+//Manipulating Complex Objects
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "NF",
+      "title": "Story",
+      "release_year": 2021,
+      "formats": [
+        "CD",
+        "Spotify",
+        "Pandora",
+        "Record",
+      ]
+    }
+  ];
